73099323dfe42baa610ccf15b6799495e13e5bae
sdongpil <pildong94@naver.com>
2024-11-08 22:47:41 +0900

    feat: 가게 이름 유효성 검사 추가 (한글, 영어, 숫자만 허용)

--- src/main/java/com/nfjs/fooddelivery/shop/service/ShopServiceImpl.java
+++ src/main/java/com/nfjs/fooddelivery/shop/service/ShopServiceImpl.java
@@ -7,6 +7,8 @@ import com.nfjs.fooddelivery.shop.repository.ShopRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.regex.Pattern;
+
 @Service
 @RequiredArgsConstructor
 public class ShopServiceImpl implements ShopService {
@@ -16,9 +18,13 @@ public class ShopServiceImpl implements ShopService {
     public ShopResponseDto createShop(ShopRequestDto requestDto) {
         // 회원 검증
 
-        Shop saveShop = shopRepository.save(Shop.toDomain(requestDto));
+        String shopName = requestDto.getName();
+        if (!Pattern.matches("^[a-zA-Z가-힣0-9]+$", shopName)) {
+            throw new IllegalStateException("가게 이름은 한글, 영어, 숫자만 포함 가능합니다.");
+        }
 
+        Shop entity = shopRepository.save(Shop.toEntity(requestDto));
 
-        return ShopResponseDto.from(saveShop);
+        return ShopResponseDto.from(entity);
     }
 }
