6904f4052928677422eb1b39f8656985c75c9d78
신영한 <syhan7516@naver.com>
2024-11-15 13:01:03 +0900

    refactor: 주문 API /api 추가

--- src/main/java/com/nfjs/fooddelivery/order/controller/OrderController.java
+++ src/main/java/com/nfjs/fooddelivery/order/controller/OrderController.java
@@ -7,16 +7,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class OrderController {
 
     private final OrderService orderService;
